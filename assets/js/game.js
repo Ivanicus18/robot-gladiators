efae977c59bfa87670c8e6868f7a96b0eb485b49
Ivan Thiessen <ivant920@gmail.com>
2021-10-18 21:52:35 -0400

    added endgame function, reset score feature, quit feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,6 +28,7 @@ var fight = function(enemyName) {
         );
         if (enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
+            playerMoney = playerMoney + 20;
             break;
         }
         else {
@@ -39,20 +40,49 @@ var fight = function(enemyName) {
         );
         if (playerHealth <= 0) {
             window.alert(playerName + " has died!");
-            window.alert("You have lost your robot in battle! Game over!")
             break;
         }
         else {
              window.alert(playerName + " still has " + playerHealth + " health left.");
         }
     }
+};
+
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game over!");
+            break;
+        }
+    }
+    // play again
+    endGame();
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
-    }
-}
+startGame();
